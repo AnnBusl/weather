22074d267d979db395ea2524c3de9b6ad62356b7
AnnBusl <annaansys@mail.ru>
2017-02-14 07:21:11 +0300

    Код информера Yandex

---
+++ web/Ya.php
@@ -0,0 +1,1 @@
+<a href="https://clck.yandex.ru/redir/dtype=stred/pid=7/cid=1228/*https://pogoda.yandex.ru/16" target="_blank"><img src="//info.weather.yandex.net/16/1.ru.png?domain=ru" border="0" alt="Яндекс.Погода"/><img width="1" height="1" src="https://clck.yandex.ru/click/dtype=stred/pid=7/cid=1227/*https://img.yandex.ru/i/pix.gif" alt="" border="0"/></a>
